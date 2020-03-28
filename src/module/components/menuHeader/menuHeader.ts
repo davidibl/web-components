ee8bfb06af41fe2e952398de66646a4bc658afd6
David Ibl <david.ibl@xnoname.com>
2020-03-28 12:23:07 +0100

    CHORE: Cleanup menu header component scss

--- src/module/components/menuHeader/menuHeader.ts
+++ src/module/components/menuHeader/menuHeader.ts
@@ -3,6 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
     selector: 'xn-menu-header',
     templateUrl: 'menuHeader.html',
+    styleUrls: ['menuHeader.scss'],
 })
 export class MenuHeaderComponent {
 
